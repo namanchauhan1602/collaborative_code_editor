9c7f4e0f806e987861dcd6edb85835dcf0ed2da2
Naman Chauhan <namanchauhan2001@gmail.com>
2024-07-30 23:20:30 +0530

    .

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const PORT = 3000;
@@ -9,48 +9,52 @@ const io = new Server(server);
 const userSocketMap = {};
 
 const getAllConnectedClients = (roomId) => {
-    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId) => {
-        return {
-            socketId: socketId,
-            username: userSocketMap[socketId],
-        };
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => {
+      return {
+        socketId: socketId,
+        username: userSocketMap[socketId],
+      };
+    }
+  );
+};
+
+io.on("connection", (socket) => {
+  // console.log('a user connected  ', socket.id);
+  socket.on("join", ({ roomId, username }) => {
+    userSocketMap[socket.id] = username;
+    socket.join(roomId);
+    const clients = getAllConnectedClients(roomId);
+    clients.map(({ socketId }) => {
+      io.to(socketId).emit("new-user-joined", {
+        clients,
+        username,
+        socketId,
+      });
     });
-}
+  });
 
-io.on('connection', (socket) => {
-    // console.log('a user connected  ', socket.id);
-    socket.on('join', ({ roomId, username }) => {
-        userSocketMap[socket.id] = username
-        socket.join(roomId);
-        const clients = getAllConnectedClients(roomId);
-        clients.map(({ socketId }) => {
-            io.to(socketId).emit('new-user-joined', {
-                clients,
-                username,
-                socketId
-            });
-        })
+  socket.on("code-change", ({ roomId, code }) => {
+    socket.in(roomId).emit("code-change", { code });
+  });
+
+  socket.on("disconnecting", () => {
+    const rooms = [...socket.rooms];
+    rooms.forEach((roomId) => {
+      socket.in(roomId).emit("disconnected", {
+        socketId: socket.id,
+        username: userSocketMap[socket.id],
+      });
     });
-
-    socket.on('code-change', ({ roomId, code }) => {
-        socket.in(roomId).emit('code-change', {code})
-    })
-
-
-    socket.on('disconnecting', () => {
-        const rooms = [...socket.rooms]
-        rooms.forEach((roomId)=>{
-            socket.in(roomId).emit('disconnected', {
-                socketId: socket.id,
-                username: userSocketMap[socket.id]
-            });
-        })
-        delete userSocketMap[socket.id];
-        socket.leave()
-    })
+    delete userSocketMap[socket.id];
+    socket.leave();
+  });
 });
 
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
