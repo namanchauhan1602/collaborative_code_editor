e77cd70be31c41448d4e3b468cfdc2e036483f3d
namanchauhan1602 <99710538+namanchauhan1602@users.noreply.github.com>
2024-08-01 11:57:55 +0530

    Update socket.js

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,11 +1,5 @@
 import { io } from 'socket.io-client';
 
 export const initSocket = async () => {
-    const options = {
-        'force new connection': true,
-        transports: ['websocket'],
-        reconnection: 'infinity',
-        timeout: 10000,
-    }
-    return io('collaborative-code-editor-server.vercel.app/', options);
+    return io('collaborative-code-editor-server.vercel.app/');
 }
